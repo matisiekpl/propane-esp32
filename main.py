aa2e00fb62c0ae44d667681d1a6a185ddeee5719
Mateusz Woźniak <matisiek11@gmail.com>
2024-05-13 20:31:13 +0200

    fix: buzzer

--- main.py
+++ main.py
@@ -9,16 +9,20 @@ import machine
 
 SSID = "iPhone (Mateusz)"
 PASSWORD = "12345678"
-ENDPOINT = "http://srv3.enteam.pl:3012/insert"
+ENDPOINT = "http://srv3.enteam.pl:3012"
 INTERVAL = 1
 
 PROPANE_PIN = 1
 AMMONIA_PIN = 2
+LED_PIN = 2
 
 # App
 
 propane_adc = machine.ADC(PROPANE_PIN)
 ammonia_adc = machine.ADC(AMMONIA_PIN)
+
+led = Pin(LED_PIN, Pin.OUT)
+
 
 def info(*message):
     output = ""
@@ -45,8 +49,9 @@ try:
 except Exception as e:
     print(e)
     machine.reset()
-    
+
 info("Time synchronized: ", time.localtime())
+
 
 def insert(propane_level, ammonia_level):
     timestamp = time.time()
@@ -56,19 +61,41 @@ def insert(propane_level, ammonia_level):
         "measured_at": timestamp,
     })
     info("Sending data: ", post_data)
-    res = urequests.post(ENDPOINT, headers={"content-type": "application/json"}, data=post_data).json()
+    res = urequests.post(ENDPOINT+'/insert', headers={
+                         "content-type": "application/json"}, data=post_data).json()
+
+
+def fetch_thresholds():
+    res = urequests.get(ENDPOINT+'/thresholds').json()
+    return res["propane_threshold"], res["ammonia_threshold"]
+
 
 def read_propane_level():
     return propane_adc.read()
 
+
 def read_ammonia_level():
     return ammonia_adc.read()
 
+
 info("Starting main loop with interval: ", INTERVAL)
+
+
+def buzz():
+    led.value(1)
+    time.sleep(1)
+    led.value(0)
+
 
 while True:
     try:
-        insert(read_propane_level(), read_ammonia_level())
+        propane_threshold, ammonia_threshold = fetch_thresholds()
+        propane_level, ammonia_level = read_propane_level(), read_ammonia_level()
+
+        insert(propane_level, ammonia_level)
+
+        if propane_level > propane_threshold or ammonia_level > ammonia_threshold:
+            buzz()
     except Exception as e:
         print(e)
     time.sleep(INTERVAL)
